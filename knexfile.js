4ca6d11c8d8170e01170ae809f09e4bebf0ba008
Tim Griffith <grifmang@gmail.com>
2020-02-02 10:38:38 -0500

    db fix

--- knexfile.js
+++ knexfile.js
@@ -34,7 +34,7 @@ module.exports = {
 
   production: {
     client: 'pg',
-    connection: process.env.DATABASE_URL,
+    connection: 'postgres://eypwkcjcjwinys:db35a44d2a2721f80aef1fcd9d3155cb5db79c28b1d3649c4ac549f120644595@ec2-52-71-122-102.compute-1.amazonaws.com:5432/d30jq9ph71ot9s',
     migrations: {
       directory: './data/migrations',
     },
@@ -43,6 +43,4 @@ module.exports = {
     },
     ssl: true,
   },
-};
-
 };
