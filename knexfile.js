120bee9f2023597698f61d687c4784f14b9f31e7
Tim Griffith <grifmang@gmail.com>
2020-02-02 10:33:17 -0500

    prod db fix

--- knexfile.js
+++ knexfile.js
@@ -1,4 +1,4 @@
-// Update with your config settings.
+require('dotenv').config();
 
 module.exports = {
 
@@ -34,18 +34,15 @@ module.exports = {
 
   production: {
     client: 'pg',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
+    connection: process.env.DATABASE_URL,
     migrations: {
-      tableName: 'knex_migrations'
-    }
-  }
+      directory: './data/migrations',
+    },
+    seeds: {
+      directory: './data/seeds',
+    },
+    ssl: true,
+  },
+};
 
 };
